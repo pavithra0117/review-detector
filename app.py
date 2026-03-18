7c603f92d47ee5fe05e348de0dbf486a806bbb54
pavithra0117 <pavithra@example.com>
2026-03-18 09:32:24 +0530

    Mobile Fix: Restore Sidebar Toggle Visibility

--- app.py
+++ app.py
@@ -155,7 +155,6 @@ st.markdown("""
     /* HIDE DEFAULTS */
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
-    header {visibility: hidden;}
     </style>
 """, unsafe_allow_html=True)
 
